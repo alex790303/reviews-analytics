013bda57648f5b7c4b4aca3a31946cc1ae56f353
Cheng Ya <chengya210@gmail.com>
2019-03-05 15:28:33 +0800

    2nd

--- read.py
+++ read.py
@@ -4,9 +4,13 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 #透過計數來看讀去狀況
-		if count % 1000 == 0: #％只是拿來求餘數，count是1000的倍數
+		if count % 10000 == 0: #％只是拿來求餘數，count是10000的倍數
 			print(len(data))
-print(len(data))
-print(data[0])
-print('-------------------------------------------')
-print(data[1])
+print('檔案讀取完畢，共計', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('留言平均長度是', sum_len/len(data))
